88683aa69d0fbe10390824b4ef219323951ff951
ethanbo-test <ethanbo.test@gmail.com>
2021-09-09 11:23:26 -0700

    try to obtain wifiManager object within callback

--- app/src/main/java/com/example/auto_setup_wifi/MainActivity.java
+++ app/src/main/java/com/example/auto_setup_wifi/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.auto_setup_wifi;
 
+import android.content.Context;
+import android.net.wifi.WifiManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -8,19 +10,51 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+  // WifiManager wifiManager = (WifiManager) this.getApplicationContext().getSystemService(
+  //     Context.WIFI_SERVICE);
+
+  protected WifiManager wifiManager;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+    // WifiManager wifiManager = (WifiManager) this.getApplicationContext().getSystemService(
+    //     Context.WIFI_SERVICE);
+    // wifiManager = (WifiManager) this.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
     Button btn1 = (Button) findViewById(R.id.btn1);
+
+    Button btn2 = (Button) findViewById(R.id.btn2);
+
+    Button btn3 = (Button) findViewById(R.id.btn3);
 
     btn1.setOnClickListener(new View.OnClickListener() {
 
+      @Override
+      public void onClick(View view) {
+        Toast.makeText(getApplicationContext(), "btn1", Toast.LENGTH_LONG).show();
+      }
+    });
+
+    btn2.setOnClickListener(new View.OnClickListener() {
+
+      String text = "btn2\n" + String.valueOf("");
+      @Override
+      public void onClick(View view) {
+        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
+      }
+    });
+
+    btn3.setOnClickListener(new View.OnClickListener() {
+      // boolean isWifiEnabled = wifiManager.isWifiEnabled();
+      // String text = "btn3\n" + String.valueOf(isWifiEnabled);
+      String text = "btn3\n";
 
       @Override
       public void onClick(View view) {
-        Toast.makeText(getApplicationContext(), "this is context", Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
       }
     });
   }

c85d7b751bea0c7b3c99ac998a02b07e9f3a6ba4
ethanbo-test <ethanbo.test@gmail.com>
2021-09-20 15:46:37 -0700

    basically works fine if wifi info is correct and wifi stays on

--- app/src/main/java/com/example/auto_setup_wifi/MainActivity.java
+++ app/src/main/java/com/example/auto_setup_wifi/MainActivity.java
@@ -1,15 +1,30 @@
 package com.example.auto_setup_wifi;
 
+import android.Manifest;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.net.ConnectivityManager;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
+import android.net.wifi.ScanResult;
+import android.net.wifi.SupplicantState;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiConfiguration.GroupCipher;
+import android.net.wifi.WifiConfiguration.KeyMgmt;
+import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
-import android.net.wifi.WifiNetworkSuggestion;
+// import android.net.wifi.WifiNetworkSuggestion;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,100 +33,93 @@ public class MainActivity extends AppCompatActivity {
   // WifiManager wifiManager = (WifiManager) this.getApplicationContext().getSystemService(
   //     Context.WIFI_SERVICE);
 
+  private Button btn1;
+  private Button btn2;
+  private Button btn3;
   protected WifiManager wifiManager;
+  private List<ScanResult> results;
+  private final String SSID1 = "Pixel_7964";
+  private final String PWD1 = "Lb8906301";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    // https://developer.aliyun.com/article/12841
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    // WifiManager wifiManager = (WifiManager) this.getApplicationContext().getSystemService(
-    //     Context.WIFI_SERVICE);
-    // wifiManager = (WifiManager) this.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+    btn1 = (Button) findViewById(R.id.btn1);
 
-    Button btn1 = (Button) findViewById(R.id.btn1);
+    btn2 = (Button) findViewById(R.id.btn2);
 
-    Button btn2 = (Button) findViewById(R.id.btn2);
+    btn3 = (Button) findViewById(R.id.btn3);
 
-    Button btn3 = (Button) findViewById(R.id.btn3);
+    wifiManager = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
 
     btn1.setOnClickListener(new View.OnClickListener() {
 
       @Override
       public void onClick(View view) {
         wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        if (wifiManager.isWifiEnabled()) {
-          System.out.println("wifi status = " + wifiManager.isWifiEnabled());
-          wifiManager.setWifiEnabled(false);
-        } else {
-          System.out.println("wifi status = " + wifiManager.isWifiEnabled());
-          wifiManager.setWifiEnabled(true);
-        }
-        Toast.makeText(getApplicationContext(), "btn1", Toast.LENGTH_LONG).show();
+
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        String ssid = wifiInfo.getSSID();
+        System.out.println("wifiInfo = " + wifiInfo);
+        System.out.println("SSID = " + ssid);
+
+        Toast.makeText(getApplicationContext(), "btn1:\nssid = " + ssid, Toast.LENGTH_LONG).show();
       }
     });
 
     btn2.setOnClickListener(new View.OnClickListener() {
-
-      String text = "btn2\n" + String.valueOf("");
       @Override
       public void onClick(View view) {
-        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        WifiConfiguration config = new WifiConfiguration();
+        config.SSID = "\"" + SSID1 + "\"";
+        config.preSharedKey = "\""+ PWD1 +"\"";
 
-        // wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        // wifiManager.setWifiEnabled(false);
-        // boolean isWifiEnabled = wifiManager.isWifiEnabled();
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
 
-        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
+        List<WifiConfiguration> list = wifiManager.getConfiguredNetworks();
+
+        if (list != null && list.size() > 0) {
+
+        }
+
+        wifiManager.addNetwork(config);
+        for (int i = 0; i < list.size(); i++) {
+          WifiConfiguration current = list.get(i);
+          System.out.println("i = " + i + "\t\tcurrent = " + current.SSID);
+          if (current.SSID != null && current.SSID.equals("\"" + SSID1 + "\"")) {
+            wifiManager.disconnect();
+            System.out.println("-------disconnecting----------");
+            wifiManager.enableNetwork(current.networkId, true);
+            wifiManager.reconnect();
+            System.out.println("-------re-connecting!!!!----------");
+
+            break;
+          }
+        }
       }
     });
 
     btn3.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-        String ssid1 = "ggg";
-        String pass1 = "sss";
+        ConnectivityManager cm = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = cm.getActiveNetworkInfo();
 
-        String ssid2 = "Pixel_7964";
-        String pass2 = "Lb890630";
-
-        final WifiNetworkSuggestion suggestion1 = new WifiNetworkSuggestion.Builder()
-            .setSsid(ssid1)
-            .setWpa2Passphrase(pass1)
-            .build();
-
-        final WifiNetworkSuggestion suggestion2 = new WifiNetworkSuggestion.Builder()
-            .setSsid(ssid2)
-            .setWpa2Passphrase(pass2)
-            .build();
-
-        final List<WifiNetworkSuggestion> lst = new ArrayList<>();
-        lst.add(suggestion1);
-        lst.add(suggestion2);
 
         wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        int status = wifiManager.addNetworkSuggestions(lst);
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        System.out.println("wifiInfo = " + wifiInfo);
 
-        System.out.println("status = " + status);
+        if (info != null && info.isConnected()) {
+          String ssid = info.getExtraInfo();
+          // NetworkCapabilities nc = cm.getNetworkInfo();
 
-        boolean isFound = false;
-        for (WifiNetworkSuggestion wifiNS : lst) {
-          System.out.println("wifiNS = " + wifiNS);
-          if (wifiNS != null && wifiNS.getSsid() != null && wifiNS.getSsid() != null) {
-            if (wifiNS.getSsid().equals(ssid2) && wifiNS.getPassphrase().equals(pass2)) {
-              isFound = true;
-              wifiManager.reconnect();
-            }
-          }
-        }
-
-        if (isFound) {
-          Toast.makeText(getApplicationContext(), "connected to hotspot", Toast.LENGTH_LONG).show();
-          System.out.println("is Found already");
+          System.out.println("ssid from btn3 = " + ssid);
         } else {
-          String text = "btn3\n";
-          Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
-          System.out.println("not found yet");
+          System.out.println("ssid from btn3 is null");
         }
       }
     });
